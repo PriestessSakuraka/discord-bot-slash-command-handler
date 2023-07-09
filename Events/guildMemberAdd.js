d88cd3ecd26402d091344a33ecabbfa1e28551cf
Manoruchan <48278137+PriestessSakuraka@users.noreply.github.com>
2023-07-09 19:27:35 +0900

    Update guildMemberAdd.js

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -1,17 +1,12 @@
 module.exports = class {
     constructor(client) {
         this.client = client
-        this.modules = client.modules
     }
 
     async run(member) {
-        const isOffline = member.guild.members.cache.get(mee6_id).presence.status === "offline" ? true : false
-
-        if (isOffline) {
-            for (const role of welcome_roles) {
-                const targetRole = member.guild.roles.cache.get(role) || member.guild.roles.cache.find(r => r.name === role)
-                if (!member.roles.cache.has(targetRole)) await member.roles.add(targetRole)
-            }
+        for (const role of this.settings.welcome_roles) {
+            const targetRole = member.guild.roles.cache.get(role) || member.guild.roles.cache.find(r => r.name === role)
+            if (!member.roles.cache.has(targetRole)) await member.roles.add(targetRole)
         }
     }
 }
